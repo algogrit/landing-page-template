5612822e19168f73e2c9e316bc3e0bff4b91489d
Gaurav Agarwal <algogrit@gmail.com>
2019-02-18 20:35:05 +0530

    Adding init script and README.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Landing Page Template
+
+Start dev server using
+
+    $ npm run dev
+
+## Automated changes
+
+    $ echo 'New Title' > TITLE
+    $ ./scripts/init.sh
---
+++ scripts/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+template=`git remote get-url origin`
+git remote add template $template
+
+title=`cat TITLE`
+
+gsed -i s/"Generic Landing Page"/"$title"/g package.json
+gsed -i s/template/"$title"/g pages/_app.js
+
+# cname=`cat CNAME`
+## TODO: Configuring netlify site name and redirection
+
+# TODO: Figure out the git URL prompt
+# git remote add origin git@gitlab.com:novel-digital-services/landing-page.git
